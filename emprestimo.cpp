2dce4b69e611b4391c9c546d92b56470a2a7168d
BrunoTdS456123 <131707629+BrunoTdS456123@users.noreply.github.com>
2024-06-18 14:21:36 +0000

    data resolvida

--- emprestimo.cpp
+++ emprestimo.cpp
@@ -2,25 +2,22 @@
 #include <string>
 #include "emprestimo.hpp"
 
+
 using namespace std;
+
 
 
 Emprestimo::Emprestimo(){};
 Emprestimo::~Emprestimo(){};
 
-void Emprestimo::SetLocacao(string dataLoc_) {
-   data_loc = dataLoc_;
-}
-void Emprestimo::SetDevolucao(string data_dev_) {
-   data_dev = data_dev_;
-}
+
 float Emprestimo::Registro(){
    std::cout << "insira o número do livro que deseja locar:";
    std::cin >> id_livro;
-   std::cout  << "insira o dia da locação:";
-   std::cin >> data_loc;
-   float DATAdev();
+   std::cout  << "dia 10 de julho";
+   data_loc=10;
+   data_dev=24;
+   std::cout << "a data de devolução do livro é:" << data_dev << "de julho";
+   
 }
-void Emprestimo::DATAdev(){
-   std::cout << "a data de devolução do livro é:" << data_dev;
-}
+
--- emprestimo.hpp
+++ emprestimo.hpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
+
 
 class Emprestimo{
     public:
     Emprestimo();
     ~Emprestimo();
 
-    void SetLocacao(string dataLoc_);
-    void SetDevolucao(string dataDev_);
+    
     float Registro();
-    void DATAdev();
+    
 
     private:
-        string data_loc;
-        string data_dev; 
-        string id_livro;
+        int data_loc;
+        int data_dev; 
+        int id_livro;
         
 };
